ddf8bd8738a0d0ae84604509149e0bf77547a078
gourav mittal <107996221+gouravmittal8081@users.noreply.github.com>
2022-08-28 00:07:37 +0530

    initial commit
    TIP_CALCULATOR

---
+++ js.js
@@ -0,0 +1,71 @@
+//Calculate Tip
+function calculateTip() {
+    var billAmt = document.getElementById("billamt").value;
+    var serviceQual = document.getElementById("serviceQual").value;
+    var numOfPeople = document.getElementById("peopleamt").value;
+  
+    //validate input
+    if (billAmt === "" || serviceQual == 0) {
+      alert("Please enter values");
+      return;
+    }
+    //Check to see if this input is empty or less than or equal to 1
+    if (numOfPeople === "" || numOfPeople <= 1) {
+      numOfPeople = 1;
+      document.getElementById("each").style.display = "none";
+    } else {
+      document.getElementById("each").style.display = "block";
+    }
+  
+    //Calculate tip
+    var total = (billAmt * serviceQual) / numOfPeople;
+    //round to two decimal places
+    total = Math.round(total * 100) / 100;
+    //next line allows us to always have two digits after decimal point
+    total = total.toFixed(2);
+    //Display the tip
+    document.getElementById("totalTip").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
+  
+  }
+  
+  //Hide the tip amount on load
+  document.getElementById("totalTip").style.display = "none";
+  document.getElementById("each").style.display = "none";
+  
+  //click to call function
+  document.getElementById("calculate").onclick = function() {
+    calculateTip();
+  
+  };
+
+
+
+
+
+
+
+
+
+
+// const btn =() =>{
+    
+//     let amount = document.getElementById('bill-amount').value;
+//    let parc = document.getElementById('tip-parc').value;
+
+    
+//    let tip = amount * (parc/100);
+//    let total = tip + Number(amount);
+   
+//    document.getElementById('tip-total').value = tip;
+//    document.getElementById('total-billed').value = total;
+
+
+//     if (amount === "" || parc == 0 )
+//        {
+//     alert("Please enter values in NUMBER");
+//     return;
+//         }
+// }
+
+
